35d6cbfce51f78173e2d263ec2bdb0e9e0a61b8c
Kamil <80323556+ksjng@users.noreply.github.com>
2025-12-10 21:44:52 +0100

    Dodano meta tagi

---
+++ components/head.php
@@ -0,0 +1,11 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="robots" content="index, follow" />
+    <meta name="description" content="Najlepsze przepisy w internecie, tylko u nas!" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script> <!-- TailwindCSS -->
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@4.0.1/dist/flowbite.min.js" rel="stylesheet" /> <!-- Flowbite -->
+    <link href="assets/css/index.css" rel="stylesheet" />
+    <title>Najlepsze przepisy</title>
+</head>
