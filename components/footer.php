5baed4a22b75aeef708eb63c2ce447a14bc1f66d
Kamil <80323556+ksjng@users.noreply.github.com>
2025-12-11 20:38:55 +0100

    Dodano stopkę

--- components/footer.php
+++ components/footer.php
@@ -1,2 +1,4 @@
-Footer
+<footer class="text-center text-sm my-1 text-gray-700">
+    © <script>document.write(new Date().getFullYear())</script> example.com
+</footer>
 <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script> <!-- Flowbite -->
