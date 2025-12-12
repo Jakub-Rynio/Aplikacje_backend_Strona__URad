6d81d98361b858195ea1d4b1a4b60987df8c3c22
Kamil <80323556+ksjng@users.noreply.github.com>
2025-12-12 21:57:59 +0100

    Usunięto niepotrzebny kod

--- blog.php
+++ blog.php
@@ -82,58 +82,6 @@
 
             </section>
 
-            <?php /* ?>
-            <?php foreach ($posts as $p): ?>
-                <div class="post">
-
-                    <h2>
-                        <a href="single.php?id=<?= (int)$p['id'] ?>">
-                            <?= get_post_title($p); ?>
-                        </a>
-                    </h2>
-                    <p>Kategoria: <?= get_post_category($p); ?></p>
-
-                    <?php if ($img = get_post_image($p)): ?>
-
-                        <a href="single.php?id=<?= (int)$p['id'] ?>">
-                            <img src="<?= $img ?>" style="max-width:300px;">
-                        </a>
-                    <?php endif; ?>
-
-                    <p><?= get_post_content($p); ?></p>
-
-                    <!-- Panel moderatora: zmiana statusu -->
-                    <?php if ($admin): ?>
-                        <?php active_switch($p); ?>
-                    <?php endif; ?>
-
-                    <!-- Komentarze do posta -->
-                    <?php 
-                        if($admin){$comments = get_comments_by_post($p['id']);} 
-                        else{
-                            $comments = get_active_comments_by_post($p['id']);
-                        };
-                        foreach ($comments as $c): ?>
-                            <div class="comment" style="margin-left:20px; border-left:2px solid #ccc; padding-left:10px;">
-                                <p><strong><?= get_comment_author($c); ?></strong> | <?= get_comment_date($c); ?></p>
-                                <p><?= get_comment_content($c); ?></p>
-                            </div>
-                            
-                    <?php if ($admin): ?>
-                        <?php active_switch_coment($c); ?>
-                    <?php endif; ?>
-                    <?php 
-                        endforeach; ?>
-
-
-                    <!-- Formularz dodawania komentarza -->
-                    <?php coment_form($p); ?>
-
-                    <hr>
-                </div>
-            <?php endforeach; ?>
-        <?php */ ?>
-
         </main>
 
         <?php include "components/footer.php" ?>
--- single.php
+++ single.php
@@ -1,26 +1,3 @@
-<?php /*
-require 'api/queries.php';
-
-$id = (int)($_GET['id'] ?? 0);
-
-$post = get_post_by_id($id);
-
-if (!$post) {
-    echo "Nie znaleziono posta";
-    exit;
-}
-?>
-
-<p>Kategoria: <?= get_post_category($post); ?></p>
-
-<?php if ($img = get_post_image($post)): ?>
-    <img src="<?= $img ?>" style="max-width:300px;">
-<?php endif; ?>
-<h1><?= get_post_title($post) ?></h1>
-<p><?= get_post_content($post) ?></p>
-<?php */ ?>
-
-
 <?php
 require 'api/functions.php';
 require 'api/queries.php';
